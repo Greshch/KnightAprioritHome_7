8a5f6939c3768b9257ea147893fc483ca04fa6c0
Greshch <greshchook@gmail.com>
2022-06-29 10:09:31 +0300

    it was done

--- KnightAprioritHome_7.cpp
+++ KnightAprioritHome_7.cpp
@@ -2,9 +2,53 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int szBoard = 8;
+    std::cout << "Enter x1: ";
+    int x1 = 0;
+    std::cin >> x1;
+    std::cout << "Enter y1: ";
+    int y1 = 0;
+    std::cin >> y1;
+
+    std::cout << "Enter x2: ";
+    int x2 = 0;
+    std::cin >> x2;
+    std::cout << "Enter y2: ";
+    int y2 = 0;
+    std::cin >> y2;
+    
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    
+    bool knight = false;
+    if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0)
+    {
+        knight = false;
+    }
+    else if (x1 >= szBoard || y1 >= szBoard || x2 >= szBoard || y2 >= szBoard)
+    {
+        knight = false;
+    }
+    else if (dx == 1 && dy == 2)
+    {
+        knight = true;
+    }
+    else if (dx == 2 && dy == 1)
+    {
+        knight = true;
+    }
+
+    if (knight)
+    {
+        std::cout << "YES\n";
+    }
+    else
+    {
+        std::cout << "NO\n";
+    }
 }
 
